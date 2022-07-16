65dad0866b3a7d59bf45196c5f3c6b470fb3b49f
David K. Tan <32520157+skytruong90@users.noreply.github.com>
2022-07-16 14:53:34 -0400

    Delete Hotel_GUI directory

--- Hotel_GUI/SwingGUI/AppSwing/src/Employees.java
+++
@@ -1,5 +0,0 @@
-public class Employees {
-    public String employeeID;
-    public String buildingCode;
-    public String roomCode;
-}
--- Hotel_GUI/src/vsu/se22/team1/Main.java
+++
@@ -1,6 +0,0 @@
-package vsu.se22.team1;
-
-public class Main {
-
-    public static void main(String[] args) {}
-}
